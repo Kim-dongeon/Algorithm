44084f29ef2d659b6bde3b13b63860f3c7a74ddd
KimDongUn <dong7061@gmail.com>
2018-09-25 09:56:03 +0900

    comb이용

--- 03 경우의 수/paren_145p.c
+++ 03 경우의 수/paren_145p.c
@@ -45,3 +45,50 @@ int main()
 	paren(n,0,arr,0);
 	return 0;
 }
+
+//----------------------comb ÀÌ¿כ
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void comb(vector<int> &v, int size, int n, int k, int to_open, int to_close)
+{
+	if (to_open < to_close)
+	{
+		return;
+	}
+	if (k == 0)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			if (v[i] == 1)
+			{
+				cout << '(';
+			}
+			else
+			{
+				cout << ')';
+			}
+		}
+		cout << endl;
+		return;
+	}
+	else if (size == n)
+	{
+		return;
+	}
+	else
+	{
+		v[size] = 1;
+		comb(v, size + 1, n, k - 1, to_open + 1, to_close);
+		v[size] = 0;
+		comb(v, size + 1, n, k, to_open, to_close + 1);
+	}
+}
+int main()
+{
+	vector<int> v(100);
+	int n;
+	cin >> n;
+	comb(v, 0, 2 * n, n, 0, 0);
+}
