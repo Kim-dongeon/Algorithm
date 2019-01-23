7099a3241318f2608a8eda1f1ff89315dae9d1ad
KimDongUn <dong7061@gmail.com>
2019-01-23 10:41:33 +0900

    Update 1057_동언.cpp

--- Study/백준/1057_동언.cpp
+++ Study/백준/1057_동언.cpp
@@ -3,7 +3,31 @@
 #include <vector>
 using namespace std;
 
+#include <iostream>
+#include <cstdio>
+#include <vector>
+using namespace std;
+
 vector<int> v;
+int main() {
+	int N, J, H;
+	scanf("%d %d %d", &N, &J, &H);
+
+	J--;
+	H--;
+	int round = 0;
+
+	while (J != H) {
+		round++;
+		J = J / 2;
+		H = H / 2;
+	}
+	printf("%d", round);
+}
+
+
+/*vector<int> v;
+
 int main() {
 	int N, J, H;
 	scanf("%d %d %d", &N, &J, &H);
@@ -34,7 +58,7 @@ int main() {
 	}
 
 }
-
+*/
 /*
 #include <cstdio>
 #include <iostream>
