5ed5451cc1d128f06a49ed95862fc45e5c63ade5
KimDongUn <dong7061@gmail.com>
2019-02-11 22:06:11 +0900

    Update array.cpp

--- 06 algorithm/array.cpp
+++ 06 algorithm/array.cpp
@@ -21,7 +21,7 @@ void multi(int temp[][6], int copy[][6], int mul, int x, int y, int n) {
 
 }
 int main() {
-	/*
+
 	int arr[][6] = {
 	{ 3, 2, 1, 2, 4, 1 },
 	{ 2, 1, 2, 1, 2, 1, },
@@ -29,32 +29,30 @@ int main() {
 	{ 2, 3, 4, 3, 4, 1, },
 	{ 3, 4, 3, 4, 3, 4, },
 	{ 2, 3, 4, 3, 1, 4 } };
-
 	int n = 6;
 	//배열 회전
 	int copy[6][6];
 	for (int i = 0; i < n; i++) {
-	for (int j = 0; j < n; j++) {
-	copy[i][j] = arr[n - 1 - j][i];
+		for (int j = 0; j < n; j++) {
+			copy[i][j] = arr[n - 1 - j][i];
+		}
 	}
-	}
-
 	for (int i = 0; i < n; i++) {
-	for (int j = 0; j < n; j++) {
-	cout << copy[i][j] << ' ';
-	}
-	cout << endl;
+		for (int j = 0; j < n; j++) {
+			cout << copy[i][j] << ' ';
+		}
+		cout << endl;
 	}
 	cout << endl;
 	//배열 달팽이
 	int dx[4] = { 0,1,0,-1 };
 	int dy[4] = { 1,0,-1,0 };
 	int direction = 0;
-	
+
 	int x = 0, y = -1;
 	int cnt = 0;
 	int val = 1;
-	int move[2] = { n, n-1 };
+	int move[2] = { n, n - 1 };
 	int idx = 0;
 	while (cnt < n*n) {
 		for (int j = 0; j < move[idx]; j++) {
@@ -66,26 +64,25 @@ int main() {
 		direction = (direction + 1) % 4;
 		move[idx]--;
 		idx = (idx + 1) % 2;
-
 	}
-		
-	
+
+
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cout << arr[i][j] << ' ';
 		}
 		cout << endl;
 	}
-	*/
+	*
 
-	//배열 확대
-	int temp[][6] = {
-		{ 0, 0, 0, 0, 0, 0 },
-		{ 0, 0, 0, 0, 0, 0, },
-		{ 0, 0, 0, 0, 0, 0 },
-		{ 0, 0, 1, 0, 0, 0, },
-		{ 0, 0, 0, 0, 0, 0, },
-		{ 0, 0, 0, 0, 0, 1 } };
+		//배열 확대
+		int temp[][6] = {
+			{ 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, },
+			{ 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 1, 0, 0, 0, },
+			{ 0, 0, 0, 0, 0, 0, },
+			{ 0, 0, 0, 0, 0, 1 } };
 
 
 	int mul = 2;
